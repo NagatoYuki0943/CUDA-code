9bf8b6c74e7824c11ddbf0b5533f85a92e05a8b8
Yuki <workcomputer.com>
2024-01-12 09:01:10 +0800

    add cuda docs

--- 1.2lesson/test.cu
+++ 1.2lesson/test.cu
@@ -1,5 +1,13 @@
 #include <stdio.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
 
+
+/**
+* __global__：表示该函数被CPU调用启动，在GPU上执行
+* __device__：表示GPU端调用且在GPU端执行的函数
+* __host__：表示CPU端调用且在CPU端执行的函数，无任何修饰符时默认是CPU端函数
+*/
 __global__ void hello_from_gpu()
 {
     printf("Hello World from the the GPU\n");
